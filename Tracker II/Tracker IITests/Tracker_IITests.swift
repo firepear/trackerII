319c47e4ae67476c36a41cfd622613f0342ce27d
Shawn Boyette <shawn@firepear.net>
2025-12-26 12:38:37 -0700

    project init

---
+++ Tracker II/Tracker IITests/Tracker_IITests.swift
@@ -0,0 +1,17 @@
+//
+//  Tracker_IITests.swift
+//  Tracker IITests
+//
+//  Created by Shawn Boyette on 12/26/25.
+//
+
+import Testing
+@testable import Tracker_II
+
+struct Tracker_IITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
